276ad6ac3eebc305c0ab81f93e7affdd21bfaa57
Wille Lee <1992season@gmail.com>
2024-05-30 01:40:30 +0900

    [EDIT] cancel tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,31 @@
 /// <reference types="vitest" />
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
-export default ({ mode }: { mode: string }) => {
-  const env = loadEnv(mode, process.cwd(), "VITE_");
-
-  return defineConfig({
-    plugins: [
-      react({
-        jsxImportSource: "@emotion/react",
-        babel: {
-          plugins: ["@emotion/babel-plugin"],
-        },
-      }),
-      tsconfigPaths(),
-    ],
-    test: {
-      environment: "jsdom",
-    },
-    server: {
-      proxy: {
-        "/api": {
-          target: "https://www.koreaexim.go.kr/site/program/financial",
-          changeOrigin: true,
-          rewrite: (path) => {
-            let apiUrl = path.replace(/^\/api/, "");
-            if (apiUrl.includes("exchangeJSON")) {
-              apiUrl += `&authkey=${env.VITE_EXCHANGE_AUTH_KEY}`;
-            }
-            return apiUrl;
-          },
+export default defineConfig({
+  plugins: [
+    react({
+      jsxImportSource: "@emotion/react",
+      babel: {
+        plugins: ["@emotion/babel-plugin"],
+      },
+    }),
+    tsconfigPaths(),
+  ],
+  test: {
+    environment: "jsdom",
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://www.koreaexim.go.kr/site/program/financial",
+        changeOrigin: true,
+        rewrite: (path) => {
+          return path.replace(/^\/api/, "");
         },
       },
     },
-  });
-};
+  },
+});
